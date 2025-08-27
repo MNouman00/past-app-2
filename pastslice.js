b0a02e6e36ff37e947132c457f67a72c003532df
Muhammad Nouman <mnouman2025o@gmail.com>
2025-08-27 23:58:01 +0500

    Add files via upload

---
+++ pastslice.js
@@ -0,0 +1,31 @@
+import { pasteSlice } from '@reduxjs/toolkit'
+
+
+const initialState = {
+    pastes :localStorage.getItem
+}
+
+export const pasteSlice = createSlice({
+  name: 'pastes',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
+
+export default counterSlice.reducer
